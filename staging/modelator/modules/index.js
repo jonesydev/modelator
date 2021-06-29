90e1b7b5aeec55cddcfd4b20b8c8ff637f3d8441
Dana <45907654+jonesydev@users.noreply.github.com>
2021-06-29 14:44:26 -0400

    Initial working prod file.

---
+++ staging/modelator/modules/index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const port = 8080;
+
+const execFile = require('child_process').execFile
+//const program = "../../../JDSstatCalc/Debug/JDSstatCalc";
+const program = "../lib/JDSstatCalc";
+
+app.use(express.static("public"));
+app.use(bodyParser.text());
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+//var types = [
+//  {
+//    title: "stats_calculator",
+//    description: "A simple statistics calulator."
+//  }
+//];
+
+//types.forEach(function (type) {
+//  app.use("/" + type.title, require("./routes/" + type.title));
+//});
+//
+//app.get("/", function (req, res) {
+//  res.render("index", {routes: types});
+//});
+
+app.post("/", (req, res) => {
+  //var data_set = req.body.data_values;
+  var data_set = req.body;
+  var results = "";
+  
+  console.log(data_set);
+  
+  var child = execFile(program, [data_set], (error, stdout, stderr) => {
+    if (error) {
+      throw error;
+    }
+    
+    results = stdout.split("\n");    
+    console.log(JSON.stringify(results));    //DEBUG ONLY 
+    res.header("Access-Control-Allow-Origin", "*");
+    res.send(JSON.stringify(results));     
+  });  
+});
+
+app.listen(port, () => {
+  console.log(`Hi! Server running at http://localhost:${port}/`);
+});
+
